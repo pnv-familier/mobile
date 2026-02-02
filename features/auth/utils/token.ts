4e320f80c56dc3e812c5bbd17823bb3796d1692a
LeKyBa <lekyba2000hc@gmail.com>
2026-02-02 17:13:45 +0700

    feature(FM-25): user logout

--- features/auth/utils/token.ts
+++ features/auth/utils/token.ts
@@ -4,4 +4,9 @@ import { AuthResponse } from "../type"
 export const saveTokens = async (data: AuthResponse) => {
     await AsyncStorage.setItem('accessToken', data.accessToken);
     await AsyncStorage.setItem('refreshToken', data.refreshToken);
-}
+};
+
+export const removeTokens = async () => {
+    await AsyncStorage.removeItem('accessToken');
+    await AsyncStorage.removeItem('refreshToken');
+};
