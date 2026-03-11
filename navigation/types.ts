86548c52e53cc1c3735202de080b888699d91409
Hồ Đức Thiện <ducthien@example.com>
2026-03-11 16:00:23 +0700

    feature(FM-113): view love task

--- navigation/types.ts
+++ navigation/types.ts
@@ -19,6 +19,8 @@ export type AppStackParamList = {
 export type TabStackParamList = {
     Home: undefined
     Chat: undefined
+    Schedule: undefined
+    LoveTasks: undefined
 }
 
 export type RootNavigationProp = NativeStackNavigationProp<RootStackParamList>
